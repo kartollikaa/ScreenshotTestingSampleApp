40bec40ce8406d2fd4ade87a432ebb5583484661
Dmitrii Maksimov <dmitrij.maksimow@gmail.com>
2024-12-16 16:55:10 +0300

    Add dependencies for roborazzi

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
   alias(libs.plugins.android.application)
   alias(libs.plugins.kotlin.android)
   alias(libs.plugins.kotlin.compose)
+  alias(libs.plugins.roborazzi)
 }
 
 android {
@@ -34,6 +35,12 @@ android {
   buildFeatures {
     compose = true
   }
+
+  testOptions {
+    unitTests {
+      isIncludeAndroidResources = true
+    }
+  }
 }
 
 dependencies {
@@ -53,4 +60,7 @@ dependencies {
   androidTestImplementation(libs.androidx.ui.test.junit4)
   debugImplementation(libs.androidx.ui.tooling)
   debugImplementation(libs.androidx.ui.test.manifest)
+  testImplementation(libs.androidx.ui.test.junit4)
+  testDebugImplementation(libs.roborazzi)
+  testDebugImplementation(libs.robolectric)
 }
